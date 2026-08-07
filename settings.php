366a67ce51b9ee5b2b1ec8e236adb439aea62bbb
yerodigo-cell <yerodigo@gmail.com>
2026-08-07 00:49:12 -0500

    Fix PHPCS warnings and errors for settings and lang files

--- settings.php
+++ settings.php
@@ -28,18 +28,23 @@ if ($hassiteconfig) {
     $settings = new admin_settingpage('local_timeshift', get_string('pluginname', 'local_timeshift'));
 
     global $CFG;
-    $logo_url = $CFG->wwwroot . '/local/timeshift/pix/icon.png';
-    
+    $logourl = $CFG->wwwroot . '/local/timeshift/pix/icon.png';
+
     $pluginname = get_string('pluginname', 'local_timeshift');
-    $lite_installed = get_string('lite_installed', 'local_timeshift');
-    $buy_pro = get_string('buy_pro', 'local_timeshift');
-    
-    $html = '<div style="text-align:center; padding:20px; background-color: #f8f9fa; border: 1px solid #dee2e6; border-radius: 8px; margin-bottom: 20px;">
-                <img src="' . $logo_url . '" alt="' . $pluginname . ' Logo" style="max-width:150px; margin-bottom:15px; border-radius: 10px; box-shadow: 0 4px 8px rgba(0,0,0,0.1);"/>
+    $liteinstalled = get_string('lite_installed', 'local_timeshift');
+    $buypro = get_string('buy_pro', 'local_timeshift');
+
+    $html = '<div style="text-align:center; padding:20px; background-color: #f8f9fa; ' .
+            'border: 1px solid #dee2e6; border-radius: 8px; margin-bottom: 20px;">
+                <img src="' . $logourl . '" alt="' . $pluginname . ' Logo" ' .
+                'style="max-width:150px; margin-bottom:15px; border-radius: 10px; box-shadow: 0 4px 8px rgba(0,0,0,0.1);"/>
                 <h3 style="color: #495057;">' . $pluginname . '</h3>
-                <p style="color: #495057; font-size: 16px;">' . $lite_installed . '</p>
+                <p style="color: #495057; font-size: 16px;">' . $liteinstalled . '</p>
                 <div style="margin-top: 20px;">
-                    <a href="https://edupluginsstudio.com/timeshift-pro.html#pricing" target="_blank" style="display: inline-block; padding: 10px 20px; background-color: #0d6efd; color: #fff; text-decoration: none; border-radius: 5px; font-weight: bold; box-shadow: 0 4px 6px rgba(13, 110, 253, 0.2); transition: background-color 0.3s;">' . $buy_pro . '</a>
+                    <a href="https://edupluginsstudio.com/timeshift-pro.html#pricing" target="_blank" ' .
+                    'style="display: inline-block; padding: 10px 20px; background-color: #0d6efd; color: #fff; ' .
+                    'text-decoration: none; border-radius: 5px; font-weight: bold; ' .
+                    'box-shadow: 0 4px 6px rgba(13, 110, 253, 0.2); transition: background-color 0.3s;">' . $buypro . '</a>
                 </div>
              </div>';
 
